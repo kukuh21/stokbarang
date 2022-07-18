df10ef9f054d4bbffc4346c7514ee9cede27d394
Kukuh21 <kaprianto@gmail.com>
2022-07-18 10:06:58 +0800

    delete

--- backup/stockbarang/resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-  <div class="pull-right hidden-xs">
-    <b>BKPSDM Tabalong</b>
-  </div>
-  <strong>Copyright &copy; {{ date('Y') }} </a></strong> {{ config('app.name') }}
-</footer>
-<div class="control-sidebar-bg"></div>
--- backup/stockbarang/resources/views/layouts/meta.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- Tell the browser to be responsive to screen width -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<link rel="icon" href="{{ asset('simpeg.ico') }}" type="image/x-icon">
